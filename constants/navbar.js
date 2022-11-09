c3aba99d9dcd8182d5f4db073afe324a53ad1e28
markomestrovic <markomestar64@gmail.com>
2022-11-09 08:19:29 +0100

    Vjezba 4: Fix navigation linking

--- constants/navbar.js
+++ constants/navbar.js
@@ -1,7 +1,7 @@
 export const navigationItems = [
     { label: 'Home', path: '/home' },
     { label: 'About us', path: '/about' },
-    { label: 'Showcase', path: '/state' },
+    { label: 'Showcase State', path: '/state' },
     { label: 'Blog', path: '' },
     { label: 'Contact', path: '' },
     { label: 'Sign in', path: '' },
